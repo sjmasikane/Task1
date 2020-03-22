2d561a89dc748e6c5250f363d50f6ae70f80ec31
Sphamandla <sphamandla@Sphamandlas-MacBook-Air.local>
2020-03-22 17:20:49 +0200

    Project commit

---
+++ src/Components/tourData.js
@@ -0,0 +1,30 @@
+export const tourData = [
+{
+ id: 1,
+ city: "new york",
+ img: "./img/newyork.jpeg",
+ name: "new your bridge tour",
+ info: "Decription goes here",
+},
+{
+ id: 2,
+ city: "paris",
+ img: "./img/paris.jpeg",
+ name: "paris lights tour",
+ info: "Decription goes here",
+},
+{
+ id: 3,
+ city: "london",
+ img: "./img/london.jpeg",
+ name: "london royal palace",
+ info: "Decription goes here",
+},
+{
+ id: 4,
+ city: "tokyo",
+ img: "./img/tokyo.jpeg",
+ name: "tokyo sushi tour",
+ info: "Decription goes here",
+},
+];
